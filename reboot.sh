f0c2ec3ac61de9f155caad064d403e2aea085d00
DevMoose-G <muse.gimicael@gmail.com>
2025-07-14 18:16:47 +0000

    add reboot stuff

--- reboot.sh
+++ reboot.sh
@@ -7,6 +7,9 @@ sudo systemctl restart fastapi.service worker.service
 
 sudo systemctl stop fastapi.service worker.service
 
+# send SIGKILL to every process in the service control group
+sudo systemctl kill --kill-who=all --signal=KILL fastapi.service
+
 # logs
 sudo systemctl status fastapi.service
 sudo journalctl -u fastapi.service -f
